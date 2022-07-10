9db22794878e08e8fc0a58d9b2e3690b96c2b134
Chris Hennick <hennickc@amazon.com>
2022-07-09 20:49:51 -0700

    Fix a logging gap

--- src/main/kotlin/io/github/pr0methean/ochd/packedimage/PngImage.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/packedimage/PngImage.kt
@@ -9,6 +9,7 @@ import javax.imageio.ImageIO
 
 class PngImage(input: Image): PackedImage {
     private val unpacked = SoftAsyncLazy(input) {
+        println("Decompressing a PNG-compressed image on demand")
         ByteArrayInputStream(packed).use { Image(it) }
     }
     override suspend fun unpacked() = unpacked.get()
