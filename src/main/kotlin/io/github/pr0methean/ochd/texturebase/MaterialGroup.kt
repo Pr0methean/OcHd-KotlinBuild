b796174d4c0b992eacd56fd639694bd47b36a363
Chris Hennick <hennickc@amazon.com>
2022-07-11 15:26:49 -0700

    Revert "Test using flattenConcat to improve hit rate on cached tasks and reduce live set"
    This reverts commit fa11b20dcb5bf4daa344a20ee99ce35b2f5133df.

--- src/main/kotlin/io/github/pr0methean/ochd/texturebase/MaterialGroup.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/texturebase/MaterialGroup.kt
@@ -5,7 +5,7 @@ import io.github.pr0methean.ochd.tasks.OutputTask
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flattenConcat
+import kotlinx.coroutines.flow.flattenMerge
 import kotlinx.coroutines.flow.map
 
 open class MaterialGroup(val elements: Flow<Material>): Material {
@@ -13,7 +13,7 @@ open class MaterialGroup(val elements: Flow<Material>): Material {
 
     @OptIn(FlowPreview::class)
     override fun outputTasks(ctx: ImageProcessingContext): Flow<OutputTask>
-            = elements.map { it.outputTasks(ctx) }.flattenConcat()
+            = elements.map { it.outputTasks(ctx) }.flattenMerge()
 }
 
 @Suppress("UNCHECKED_CAST")
