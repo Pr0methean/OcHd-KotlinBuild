9c75a65d7725900da7fb93b645c84b9be3f34453
Chris Hennick <hennickc@amazon.com>
2022-07-11 16:49:07 -0700

    Limit OutputTask parallelism

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/OutputTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/OutputTask.kt
@@ -2,9 +2,12 @@ package io.github.pr0methean.ochd.tasks
 
 import io.github.pr0methean.ochd.ImageProcessingContext
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.withContext
 import java.util.*
 
+@OptIn(ExperimentalCoroutinesApi::class)
+val ioContext = Dispatchers.IO.limitedParallelism(16)
 data class OutputTask(private val producer: TextureTask,
                       val name: String,
                       val ctx: ImageProcessingContext
@@ -19,7 +22,7 @@ data class OutputTask(private val producer: TextureTask,
             println("Skipping $name because it's not implemented yet")
         }
     }
-    suspend fun run() = withContext(Dispatchers.IO) {
+    suspend fun run() = withContext(ioContext) {
         ctx.onTaskLaunched(this@OutputTask)
         file.parentFile.mkdirs()
         ctx.retrying(name) {invoke()}
