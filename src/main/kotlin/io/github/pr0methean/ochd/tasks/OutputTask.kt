37e4ba3002cc8e2c9d1489761f8ba2e55271f0b4
Chris Hennick <hennickc@amazon.com>
2022-07-11 21:14:57 -0700

    Refactor OutputTask; fix misleading logging

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/OutputTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/OutputTask.kt
@@ -16,12 +16,7 @@ data class OutputTask(private val producer: TextureTask,
     val file by lazy {ctx.outTextureRoot.resolve(name.lowercase(Locale.ENGLISH) + ".png")}
     suspend fun invoke() {
         try {
-            val image = if (ctx.needSemaphore && (producer.willExpandHeap()
-                        || producer.isComplete() && !runBlocking{producer.getImage()}.isAlreadyPacked())) {
-                ctx.newTasksSemaphore.withPermit {producer.getImage()}
-            } else {
-                producer.getImage()
-            }
+            val image = producer.getImage()
             image.writePng(file)
         } catch (e: NotImplementedError) {
             println("Skipping $name because it's not implemented yet")
@@ -29,11 +24,19 @@ data class OutputTask(private val producer: TextureTask,
     }
     suspend fun run() {
         withContext(Dispatchers.IO) {
-            ctx.onTaskLaunched(this@OutputTask)
             file.parentFile.mkdirs()
+            if (ctx.needSemaphore && (producer.willExpandHeap()
+                        || producer.isComplete() && !runBlocking{producer.getImage()}.isAlreadyPacked())) {
+                ctx.newTasksSemaphore.withPermit {
+                    ctx.onTaskLaunched(this@OutputTask)
+                    ctx.retrying(name) {invoke()}
+                }
+            } else {
+                ctx.onTaskLaunched(this@OutputTask)
+                ctx.retrying(name) {invoke()}
+            }
+            ctx.onTaskCompleted(this@OutputTask)
         }
-        ctx.retrying(name) {invoke()}
-        ctx.onTaskCompleted(this@OutputTask)
     }
 
     override fun toString(): String = "OutputTask for $name"
