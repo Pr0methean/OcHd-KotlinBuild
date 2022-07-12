fb27abcf83948775937f9fce607b53b9ffd77131
Chris Hennick <hennickc@amazon.com>
2022-07-11 19:43:02 -0700

    Make PNG compression asynchronous

--- src/main/kotlin/io/github/pr0methean/ochd/packedimage/PngImage.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/packedimage/PngImage.kt
@@ -4,6 +4,7 @@ import io.github.pr0methean.ochd.ImageProcessingContext
 import io.github.pr0methean.ochd.SoftAsyncLazy
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.image.Image
+import kotlinx.coroutines.async
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
@@ -11,14 +12,17 @@ import javax.imageio.ImageIO
 class PngImage(input: Image, val ctx: ImageProcessingContext, val name: String): PackedImage {
     private val unpacked = SoftAsyncLazy(input) {
         println("Decompressing from PNG: $name")
-        ctx.retrying("Decompression of $name") {ByteArrayInputStream(packed).use { Image(it) }}
+        ctx.retrying("Decompression of $name") {ByteArrayInputStream(packed()).use { Image(it) }}
     }
     override suspend fun unpacked() = unpacked.get()
-    private val packed = ByteArrayOutputStream().use {
-        println("Compressing to PNG: $name")
-        ImageIO.write(SwingFXUtils.fromFXImage(input, null), "PNG", it)
-        return@use it.toByteArray()
+    private val packingTask = ctx.scope.async {
+        ByteArrayOutputStream().use {
+            println("Compressing to PNG: $name")
+            ImageIO.write(SwingFXUtils.fromFXImage(input, null), "PNG", it)
+            println("Done compressing to PNG: $name")
+            return@async it.toByteArray()
+        }
     }
 
-    override suspend fun packed(): ByteArray = packed
+    override suspend fun packed(): ByteArray = packingTask.await()
 }
