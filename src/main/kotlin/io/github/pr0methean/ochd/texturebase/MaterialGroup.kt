680810a19dd217d2c67d4990b2c57cd09dea0193
Chris Hennick <hennickc@amazon.com>
2022-07-16 14:07:02 -0700

    Test limiting concurrency to ensure related blocks are generated concurrently when possible

--- src/main/kotlin/io/github/pr0methean/ochd/texturebase/MaterialGroup.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/texturebase/MaterialGroup.kt
@@ -3,19 +3,20 @@ package io.github.pr0methean.ochd.texturebase
 import io.github.pr0methean.ochd.ImageProcessingContext
 import io.github.pr0methean.ochd.tasks.OutputTask
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flattenMerge
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 
-open class MaterialGroup(val elements: Flow<Material>): Material {
+@OptIn(FlowPreview::class)
+open class MaterialGroup(val elements: Flow<Material>, val concurrency: Int = DEFAULT_CONCURRENCY): Material {
+    constructor(concurrency: Int, vararg elements: Material): this(elements.asFlow(), concurrency)
+
     constructor(vararg elements: Material): this(elements.asFlow())
 
     @OptIn(FlowPreview::class)
     override fun outputTasks(ctx: ImageProcessingContext): Flow<OutputTask>
-            = elements.map { it.outputTasks(ctx) }.flattenMerge()
+            = elements.map { it.outputTasks(ctx) }.flattenMerge(concurrency)
 }
 
+@OptIn(FlowPreview::class)
 @Suppress("UNCHECKED_CAST")
-inline fun <reified E : Enum<out Material>> group()
-        = MaterialGroup(E::class.java.enumConstants.asFlow() as Flow<Material>)
+inline fun <reified E : Enum<out Material>> group(concurrency: Int = DEFAULT_CONCURRENCY)
+        = MaterialGroup(E::class.java.enumConstants.asFlow() as Flow<Material>, concurrency)
