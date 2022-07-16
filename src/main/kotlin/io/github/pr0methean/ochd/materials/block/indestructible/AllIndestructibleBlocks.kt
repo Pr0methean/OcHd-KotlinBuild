73e833ca054c7e87c310956bbad38b50658bd3c3
Chris Hennick <hennickc@amazon.com>
2022-07-16 14:57:15 -0700

    Generate indestructible-block groups separately

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/indestructible/AllIndestructibleBlocks.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/indestructible/AllIndestructibleBlocks.kt
@@ -2,4 +2,4 @@ package io.github.pr0methean.ochd.materials.block.indestructible
 
 import io.github.pr0methean.ochd.texturebase.MaterialGroup
 
-val INDESTRUCTIBLE_BLOCKS = MaterialGroup(COMMAND_BLOCKS, STRUCTURE_AND_JIGSAW_BLOCKS, SIMPLE_INDESTRUCTIBLE_BLOCKS)
+val INDESTRUCTIBLE_BLOCKS = MaterialGroup(1, COMMAND_BLOCKS, STRUCTURE_AND_JIGSAW_BLOCKS, SIMPLE_INDESTRUCTIBLE_BLOCKS)
