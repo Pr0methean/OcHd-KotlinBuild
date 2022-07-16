678605d3e8b48ce59386adf179627cfa02462953
Chris Hennick <hennickc@amazon.com>
2022-07-16 15:50:32 -0700

    Move related PICKAXE_BLOCKS subgroups to be adjacent

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/AllPickaxeBlocks.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/AllPickaxeBlocks.kt
@@ -2,6 +2,6 @@ package io.github.pr0methean.ochd.materials.block.pickaxe
 
 import io.github.pr0methean.ochd.texturebase.MaterialGroup
 
-val PICKAXE_BLOCKS = MaterialGroup(1, ORE_BASES, ORES, COPPER_OXIDES, SIMPLE_PICKAXE_BLOCKS, CutCopper,
+val PICKAXE_BLOCKS = MaterialGroup(1, ORE_BASES, ORES, COPPER_OXIDES, CutCopper, SIMPLE_PICKAXE_BLOCKS,
         StainedGlassFront, StainedGlassTop, Glass, NYLIUMS, POLISHABLES, Concrete, BoneBlock, Rails, MiscRedstone,
         Furnace)
