7f14a28ab031f50c8ed4cfd39cce759e344d804f
Chris Hennick <hennickc@amazon.com>
2022-07-16 15:47:24 -0700

    More concurrency tweaks

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/AllPickaxeBlocks.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/AllPickaxeBlocks.kt
@@ -2,5 +2,6 @@ package io.github.pr0methean.ochd.materials.block.pickaxe
 
 import io.github.pr0methean.ochd.texturebase.MaterialGroup
 
-val PICKAXE_BLOCKS = MaterialGroup(1, ORES, COPPER_OXIDES, ORE_BASES, SIMPLE_PICKAXE_BLOCKS, CutCopper, StainedGlassFront, NYLIUMS,
-        POLISHABLES, Concrete, BoneBlock, Rails, MiscRedstone, Furnace, Glass, StainedGlassFront, StainedGlassTop)
+val PICKAXE_BLOCKS = MaterialGroup(1, ORE_BASES, ORES, COPPER_OXIDES, SIMPLE_PICKAXE_BLOCKS, CutCopper,
+        StainedGlassFront, StainedGlassTop, Glass, NYLIUMS, POLISHABLES, Concrete, BoneBlock, Rails, MiscRedstone,
+        Furnace)
