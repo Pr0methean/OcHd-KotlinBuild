e86be0f9d09cd2d073196e02bc3016226ba66d71
Chris Hennick <hennickc@amazon.com>
2022-07-16 14:38:41 -0700

    Tweak concurrency limits

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/axe/AllAxeBlocks.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/axe/AllAxeBlocks.kt
@@ -2,4 +2,4 @@ package io.github.pr0methean.ochd.materials.block.axe
 
 import io.github.pr0methean.ochd.texturebase.MaterialGroup
 
-val AXE_BLOCKS = MaterialGroup(2, WOODS, GIANT_MUSHROOMS, SIMPLE_AXE_BLOCKS)
+val AXE_BLOCKS = MaterialGroup(1, WOODS, GIANT_MUSHROOMS, SIMPLE_AXE_BLOCKS)
