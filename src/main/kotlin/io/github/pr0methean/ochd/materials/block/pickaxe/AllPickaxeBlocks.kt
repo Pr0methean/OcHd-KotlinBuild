d5d85573fc0d92f467dc637c809d9f28d2fef7ef
Chris Hennick <hennickc@amazon.com>
2022-07-16 15:08:15 -0700

    Test a concurrency limit of 1 for pickaxe blocks

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/AllPickaxeBlocks.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/AllPickaxeBlocks.kt
@@ -2,5 +2,5 @@ package io.github.pr0methean.ochd.materials.block.pickaxe
 
 import io.github.pr0methean.ochd.texturebase.MaterialGroup
 
-val PICKAXE_BLOCKS = MaterialGroup(2, ORES, COPPER_OXIDES, ORE_BASES, SIMPLE_PICKAXE_BLOCKS, CutCopper, StainedGlassFront, NYLIUMS,
+val PICKAXE_BLOCKS = MaterialGroup(1, ORES, COPPER_OXIDES, ORE_BASES, SIMPLE_PICKAXE_BLOCKS, CutCopper, StainedGlassFront, NYLIUMS,
         POLISHABLES, Concrete, BoneBlock, Rails, MiscRedstone, Furnace, Glass, StainedGlassFront, StainedGlassTop)
