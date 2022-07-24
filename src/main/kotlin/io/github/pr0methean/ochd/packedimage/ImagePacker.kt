1f36f4f0793de37a6da6cb98ca112224ab3c3bd2
Chris Hennick <hennickc@amazon.com>
2022-07-23 18:36:07 -0700

    Test with unconstrained deduplication

--- src/main/kotlin/io/github/pr0methean/ochd/packedimage/ImagePacker.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/packedimage/ImagePacker.kt
@@ -8,21 +8,20 @@ import javafx.scene.image.Image
 import kotlinx.coroutines.CoroutineScope
 
 const val MAX_UNCOMPRESSED_TILESIZE = 512
-const val MIN_SIZE_TO_DEDUP = 32
-private const val DEDUP_CACHE_SIZE = 5_000L
+const val MIN_SIZE_TO_DEDUP = 1
 
 class ImagePacker(
     val scope: CoroutineScope, private val retryer: Retryer, private val stats: ImageProcessingStats,
     private val maxQuadtreeDepth: Int,
     val leafSize: Int
 ) {
-    private val deduplicationMap: LoadingCache<ImageNode, ImageNode> = Caffeine.newBuilder().maximumSize(DEDUP_CACHE_SIZE).build<ImageNode, ImageNode> { it }
+    private val deduplicationCache: LoadingCache<ImageNode, ImageNode> = Caffeine.newBuilder().build<ImageNode, ImageNode> { it }
 
     @Suppress("UNCHECKED_CAST")
     suspend fun <T : ImageNode> deduplicate(input: T): T {
         if (input.shouldDeduplicate()
                && input.height >= MIN_SIZE_TO_DEDUP) {
-            val original = deduplicationMap.get(input)
+            val original = deduplicationCache.get(input)
             if (original !== input) {
                 original.mergeWithDuplicate(input)
                 return original as T
