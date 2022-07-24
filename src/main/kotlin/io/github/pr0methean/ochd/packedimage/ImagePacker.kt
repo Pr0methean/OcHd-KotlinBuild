bd7c174acc52205f4b458df232c909bbb9b56764
Chris Hennick <hennickc@amazon.com>
2022-07-23 21:24:38 -0700

    Fix bad merge

--- src/main/kotlin/io/github/pr0methean/ochd/packedimage/ImagePacker.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/packedimage/ImagePacker.kt
@@ -1,11 +1,10 @@
 package io.github.pr0methean.ochd.packedimage
 
-import com.github.benmanes.caffeine.cache.Caffeine
-import com.github.benmanes.caffeine.cache.LoadingCache
 import io.github.pr0methean.ochd.ImageProcessingStats
 import io.github.pr0methean.ochd.Retryer
 import javafx.scene.image.Image
 import kotlinx.coroutines.CoroutineScope
+import java.util.concurrent.ConcurrentHashMap
 
 const val MAX_UNCOMPRESSED_TILESIZE = 512
 const val MIN_SIZE_TO_DEDUP = 128
@@ -15,14 +14,14 @@ class ImagePacker(
     private val maxQuadtreeDepth: Int,
     val leafSize: Int
 ) {
-    private val nodeDedupCache: LoadingCache<ImageNode, ImageNode> = Caffeine.newBuilder().build<ImageNode, ImageNode> { it }
+    private val nodeDedupCache = ConcurrentHashMap<ImageNode, ImageNode>()
 
     @Suppress("UNCHECKED_CAST")
     suspend fun <T : ImageNode> deduplicate(input: T): T {
         if (input.shouldDeduplicate()
                && input.height >= MIN_SIZE_TO_DEDUP) {
-            val original = nodeDedupCache.get(input)
-            if (original !== input) {
+            val original = nodeDedupCache.putIfAbsent(input, input)
+            if (original != null) {
                 original.mergeWithDuplicate(input)
                 return original as T
             }
