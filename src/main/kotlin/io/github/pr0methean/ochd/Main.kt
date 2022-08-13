cea990d0780febcda27e8574ee26760303b6617f
Chris Hennick <hennickc@amazon.com>
2022-08-13 15:14:54 -0700

    Start cleanup and metadata copying sooner and as one task

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.toList
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.util.Unbox.box
-import java.lang.ref.WeakReference
 import java.nio.file.Paths
 import java.util.concurrent.ConcurrentLinkedDeque
 import java.util.concurrent.atomic.LongAdder
@@ -27,17 +26,28 @@ suspend fun main(args: Array<String>) {
         println("Usage: main <size>")
         return
     }
-    Platform.startup {}
     val supervisorJob = SupervisorJob()
+    val ioScope = CoroutineScope(Dispatchers.IO).plus(supervisorJob)
+    val out = Paths.get("pngout").toAbsolutePath().toFile()
+    val metadataDirectory = Paths.get("metadata").toAbsolutePath().toFile()
+    val cleanupAndCopyMetadata = ioScope.launch(CoroutineName("Delete old outputs & copy metadata files")) {
+        out.deleteRecursively()
+        metadataDirectory.walkTopDown().forEach {
+            val outputPath = out.resolve(it.relativeTo(metadataDirectory))
+            if (it.isDirectory) {
+                outputPath.mkdirs()
+            } else {
+                it.copyTo(outputPath)
+            }
+        }
+    }
+    Platform.startup {}
     val tileSize = args[0].toInt()
     if (tileSize <= 0) throw IllegalArgumentException("tileSize shouldn't be zero or negative but was ${args[0]}")
     val scope = CoroutineScope(Dispatchers.Default).plus(supervisorJob)
     val svgDirectory = Paths.get("svg").toAbsolutePath().toFile()
-    val metadataDirectory = Paths.get("metadata").toAbsolutePath().toFile()
-    val out = Paths.get("pngout").toAbsolutePath().toFile()
     val outTextureRoot = out.resolve("assets").resolve("minecraft").resolve("textures")
-    val ioScope = CoroutineScope(Dispatchers.IO).plus(supervisorJob)
-    val cleanupJob = ioScope.launch(CoroutineName("Delete old outputs")) { out.deleteRecursively() }
+
     val ctx = ImageProcessingContext(
         name = "MainContext",
         tileSize = tileSize,
@@ -51,24 +61,10 @@ suspend fun main(args: Array<String>) {
     startMonitoring(stats, scope)
     var attemptNumber = 1
     val time = measureNanoTime {
-        val copyMetadata = ioScope.plus(CoroutineName("Copy metadata files")).launch {
-            cleanupJob.join()
-            stats.onTaskLaunched("Copy metadata files", "Copy metadata files")
-            metadataDirectory.walkTopDown().forEach {
-                val outputPath = out.resolve(it.relativeTo(metadataDirectory))
-                if (it.isDirectory) {
-                    outputPath.mkdirs()
-                } else {
-                    it.copyTo(outputPath)
-                }
-            }
-            stats.onTaskCompleted("Copy metadata files", "Copy metadata files")
-        }
-        val copyMetadataWeakRef = WeakReference(copyMetadata)
         stats.onTaskLaunched("Build task graph", "Build task graph")
         var tasks = ALL_MATERIALS.outputTasks(ctx).toList()
         stats.onTaskCompleted("Build task graph", "Build task graph")
-        cleanupJob.join()
+        cleanupAndCopyMetadata.join()
         System.gc()
         val tasksRun = LongAdder()
         while (tasks.isNotEmpty()) {
@@ -96,7 +92,6 @@ suspend fun main(args: Array<String>) {
             }
             tasks = tasksToRetry.toList()
             if (tasksToRetry.isNotEmpty()) {
-                copyMetadata.join()
                 System.gc()
                 logger.warn(
                     "{} tasks succeeded and {} failed on attempt {}",
@@ -106,7 +101,6 @@ suspend fun main(args: Array<String>) {
                 attemptNumber++
             }
         }
-        copyMetadataWeakRef.get()?.join()
     }
     stopMonitoring()
     Platform.exit()
