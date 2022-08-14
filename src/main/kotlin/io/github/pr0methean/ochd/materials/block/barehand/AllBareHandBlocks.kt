2e07b81cfae2e55335de18111eb1e80a53d09117
Chris Hennick <hennickc@amazon.com>
2022-08-14 12:06:12 -0700

    Add cave vines

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/barehand/AllBareHandBlocks.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/barehand/AllBareHandBlocks.kt
@@ -4,6 +4,6 @@ import io.github.pr0methean.ochd.texturebase.MaterialGroup
 import io.github.pr0methean.ochd.texturebase.group
 
 val BARE_HAND_BLOCKS = MaterialGroup(
-    Tnt, Wool, Torch, group<Crop>(), group<BiomeColorizedPlant>(), group<SimpleBareHandBlock>(),
+    CaveVines, Tnt, Wool, Torch, group<Crop>(), group<BiomeColorizedPlant>(), group<SimpleBareHandBlock>(),
     group<DoubleTallFlower>()
 )
---
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/barehand/CaveVines.kt
@@ -0,0 +1,41 @@
+package io.github.pr0methean.ochd.materials.block.barehand
+
+import io.github.pr0methean.ochd.ImageProcessingContext
+import io.github.pr0methean.ochd.c
+import io.github.pr0methean.ochd.tasks.consumable.OutputTask
+import io.github.pr0methean.ochd.texturebase.ShadowHighlightMaterial
+import javafx.scene.paint.Paint
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+val berryHighlight = c(0xffff6b)
+val berryColor = c(0xeb8931)
+object CaveVines: ShadowHighlightMaterial {
+    override val color: Paint = c(0x507233)
+    override val shadow: Paint = c(0x4f3200)
+    override val highlight: Paint = c(0x70922d)
+    override suspend fun outputTasks(ctx: ImageProcessingContext): Flow<OutputTask> = flow {
+        val vinePlantTask = ctx.stack {
+            layer("wavyVines", shadow)
+            layer("waves", highlight)
+        }
+        val vineTask = ctx.stack {
+            layer("wavyVinesBottom", shadow)
+            layer("wavesBottom", highlight)
+        }
+        val berryTask = ctx.stack {
+            layer("vineBerries", berryColor)
+            layer("vineBerriesHighlight", berryHighlight)
+        }
+        emit(ctx.out(vinePlantTask, "block/cave_vines_plant"))
+        emit(ctx.out(ctx.stack {
+            copy(vinePlantTask)
+            copy(berryTask)
+        }, "block/cave_vines_plant_lit"))
+        emit(ctx.out(vineTask, "block/cave_vines"))
+        emit(ctx.out(ctx.stack {
+            copy(vineTask)
+            copy(berryTask)
+        }, "block/cave_vines_lit"))
+    }
+}
