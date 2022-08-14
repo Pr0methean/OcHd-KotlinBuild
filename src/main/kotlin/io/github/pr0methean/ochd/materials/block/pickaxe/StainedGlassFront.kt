c9e42e0dd8f4e8ce3503c33a0dce008000de6cea
Chris Hennick <hennickc@amazon.com>
2022-08-14 14:03:02 -0700

    Test with a refactored masterTask for StainedGlassFront

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/StainedGlassFront.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/StainedGlassFront.kt
@@ -1,25 +1,25 @@
 package io.github.pr0methean.ochd.materials.block.pickaxe
 
 import io.github.pr0methean.ochd.ImageProcessingContext
-import io.github.pr0methean.ochd.LayerListBuilder
-import io.github.pr0methean.ochd.tasks.consumable.ImageTask
+import io.github.pr0methean.ochd.materials.DYES
 import io.github.pr0methean.ochd.tasks.consumable.OutputTask
-import io.github.pr0methean.ochd.texturebase.DyedBlock
+import io.github.pr0methean.ochd.tasks.consumable.RepaintTask
+import io.github.pr0methean.ochd.tasks.consumable.caching.noopTaskCache
+import io.github.pr0methean.ochd.texturebase.Material
 import javafx.scene.paint.Color
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-object StainedGlassFront: DyedBlock("stained_glass") {
-    override suspend fun LayerListBuilder.createTextureLayers(color: Color) {
-        layer(masterTask, color)
-    }
-    @Volatile lateinit var masterTask: ImageTask
+object StainedGlassFront: Material {
 
-    override suspend fun outputTasks(ctx: ImageProcessingContext): Flow<OutputTask> {
-        masterTask = ctx.stack {
+    override suspend fun outputTasks(ctx: ImageProcessingContext): Flow<OutputTask> = flow {
+        val masterTask = ctx.stack {
             background(Color.BLACK, 0.25)
             layer("borderSolid", Color.BLACK)
             layer("streaks", Color.BLACK)
         }
-        return super.outputTasks(ctx)
+        DYES.forEach { (name, color) ->
+            emit(ctx.out("block/${name}_stained_glass", RepaintTask(masterTask, color, 1.0, noopTaskCache(), ctx.stats)))
+        }
     }
 }
