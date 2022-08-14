e76ebf29e14b5e244e99df121b142b31c664a749
Chris Hennick <hennickc@amazon.com>
2022-08-14 11:30:05 -0700

    Add vines

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/shears/SimpleShearBlock.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/shears/SimpleShearBlock.kt
@@ -1,6 +1,7 @@
 package io.github.pr0methean.ochd.materials.block.shears
 
 import io.github.pr0methean.ochd.LayerListBuilder
+import io.github.pr0methean.ochd.materials.block.shovel.DirtGroundCover
 import io.github.pr0methean.ochd.texturebase.Block
 import io.github.pr0methean.ochd.texturebase.SingleTextureMaterial
 import javafx.scene.paint.Color
@@ -9,15 +10,15 @@ import javafx.scene.paint.Color
 enum class SimpleShearBlock: SingleTextureMaterial, Block {
     COBWEB {
         override suspend fun LayerListBuilder.createTextureLayers() {
-            /*
-            push ringsCentralBullseye ${white} cobweb1
-push x ${white} cobweb2
-push cross ${white} cobweb3
-out_stack block/cobweb
-             */
             layer("ringsCentralBullseye", Color.WHITE, 0.75)
             layer("x", Color.WHITE, 0.85)
             layer("cross", Color.WHITE, 0.85)
+        }
+    },
+    VINE {
+        override suspend fun LayerListBuilder.createTextureLayers() {
+            layer("wavyVines", DirtGroundCover.GRASS_BLOCK.highlight)
+            layer("waves", DirtGroundCover.GRASS_BLOCK.shadow)
         }
     };
 }
