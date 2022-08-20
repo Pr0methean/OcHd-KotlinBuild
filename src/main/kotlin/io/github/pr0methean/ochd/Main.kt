a558116a760ea66e8a2aaf3ada70ac7e21751ad7
Chris Hennick <hennickc@amazon.com>
2022-08-19 17:11:10 -0700

    Revert "Bug fix: distance function should account for size of second task"
    This reverts commit d37a8f504fa6e22b14613c71f1701f1eeef2e98e.

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -64,7 +64,7 @@ suspend fun main(args: Array<String>) {
     val time = measureNanoTime {
         stats.onTaskLaunched("Build task graph", "Build task graph")
         var tasks = ALL_MATERIALS.outputTasks(ctx).toList()
-        val distances = WeakHashMap<OutputTask,WeakHashMap<OutputTask, Double>>()
+        val distances = WeakHashMap<OutputTask,WeakHashMap<OutputTask, Int>>()
         for (task in tasks) {
             distances[task] = WeakHashMap()
             for (otherTask in tasks) {
@@ -131,8 +131,8 @@ suspend fun main(args: Array<String>) {
     exitProcess(0)
 }
 
-fun distanceFrom(first: OutputTask, second: OutputTask): Double {
+fun distanceFrom(first: OutputTask, second: OutputTask): Int {
     val task1deps = first.andAllDependencies()
     val task2deps = second.andAllDependencies()
-    return task2deps.minus(task1deps).size.toDouble() / task2deps.size
+    return task2deps.minus(task1deps).size
 }
