fa575e9bf483f3813ea88891d2309dd848cd82b0
Chris Hennick <hennickc@amazon.com>
2022-08-20 14:52:48 -0700

    Reusable variable for dyed terracotta shared layers

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/DyedTerracotta.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/DyedTerracotta.kt
@@ -1,18 +1,28 @@
 package io.github.pr0methean.ochd.materials.block.pickaxe
 
+import io.github.pr0methean.ochd.ImageProcessingContext
 import io.github.pr0methean.ochd.LayerListBuilder
 import io.github.pr0methean.ochd.materials.block.pickaxe.SimplePickaxeBlock.TERRACOTTA
+import io.github.pr0methean.ochd.tasks.ImageTask
+import io.github.pr0methean.ochd.tasks.OutputTask
 import io.github.pr0methean.ochd.texturebase.DyedBlock
 import javafx.scene.paint.Color
+import kotlinx.coroutines.flow.Flow
 
-object DyedTerracotta: DyedBlock("terracotta") {
-    override suspend fun LayerListBuilder.createTextureLayers(color: Color) {
-        background(color)
-        copy {
+object DyedTerracotta : DyedBlock("terracotta") {
+    var masterTask: ImageTask? = null
+    override suspend fun outputTasks(ctx: ImageProcessingContext): Flow<OutputTask> {
+        masterTask = ctx.stack {
             layer("bigRingsTopLeftBottomRight", TERRACOTTA.highlight)
             layer("bigDotsBottomLeftTopRight", TERRACOTTA.shadow)
             layer("bigRingsBottomLeftTopRight", TERRACOTTA.highlight)
             layer("borderRoundDots", TERRACOTTA.color)
         }
+        return super.outputTasks(ctx)
+    }
+
+    override suspend fun LayerListBuilder.createTextureLayers(color: Color) {
+        background(color)
+        copy(masterTask!!)
     }
 }
