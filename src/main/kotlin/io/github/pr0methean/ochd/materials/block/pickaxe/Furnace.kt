393dcf8e488727b2632a0909e3b4f2fb96da0ccc
Chris Hennick <hennickc@amazon.com>
2022-08-20 19:30:50 -0700

    Add Blast Furnace

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/Furnace.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/Furnace.kt
@@ -23,5 +23,32 @@ object Furnace: Material {
             copy(furnaceSide)
             layer("furnaceFrontLit")
         }, "block/furnace_front_on"))
+        emit(ctx.out({
+            background(OreBase.STONE.shadow)
+            layer("cornerCrosshairs", OreBase.stoneExtremeHighlight)
+        }, "block/blast_furnace_top"))
+        val blastFurnaceSide = ctx.stack {
+            copy(furnaceSide)
+            layer("cornerCrosshairs", OreBase.stoneExtremeHighlight)
+        }
+        emit(ctx.out(blastFurnaceSide, "block/blast_furnace_side"))
+        val blastFurnaceFrontBase = ctx.stack {
+            copy(blastFurnaceSide)
+            layer("commandBlockSquare", OreBase.stoneExtremeHighlight)
+        }
+        emit(ctx.out({
+            copy(blastFurnaceFrontBase)
+            layer("blastFurnaceHoles")
+        }, "block/blast_furnace_front"))
+        emit(ctx.out(ctx.animate(listOf(
+            ctx.stack {
+                copy(blastFurnaceFrontBase)
+                layer("blastFurnaceHolesLit")
+            },
+            ctx.stack {
+                copy(blastFurnaceFrontBase)
+                layer("blastFurnaceHolesLit1")
+            }
+        )), "block/blast_furnace_front_on"))
     }
 }
