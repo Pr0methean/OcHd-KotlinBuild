72379e8f0499a336329fa5ecf27c03e913124f52
Chris Hennick <hennickc@amazon.com>
2022-09-04 21:34:39 -0700

    Revert "Bug fix: remove unused SemiStrongTaskCache.kt"
    This reverts commit bbda5b207182b3d161880192682adb35eb691d81.

---
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
@@ -0,0 +1,29 @@
+package io.github.pr0methean.ochd.tasks.caching
+
+import com.github.benmanes.caffeine.cache.Cache
+
+class SemiStrongTaskCache<T>(name: String, private val backingCache: Cache<SemiStrongTaskCache<*>, Result<*>>):
+        WeakTaskCache<T>(name) {
+    override var enabled: Boolean
+        get() = super.enabled
+        set(value) {
+            super.enabled = value
+            if (!value) {
+                backingCache.invalidate(this)
+            }
+        }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun getNow(): Result<T>? {
+        return backingCache.getIfPresent(this) as Result<T>? ?: super.getNow()
+    }
+
+    override fun enabledSet(value: Result<T>?) {
+        if (value == null) {
+            backingCache.invalidate(this)
+        } else {
+            backingCache.put(this, value)
+        }
+        super.enabledSet(value)
+    }
+}
