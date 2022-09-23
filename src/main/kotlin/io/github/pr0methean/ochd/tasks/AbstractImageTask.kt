b163b0000858fdb3c1564323592f6a87df5de9b3
Chris Hennick <hennickc@amazon.com>
2022-09-23 09:16:27 -0700

    Optimization: Opaque repaint can be reused by another RepaintTask for same base image

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/AbstractImageTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/AbstractImageTask.kt
@@ -1,15 +1,25 @@
 package io.github.pr0methean.ochd.tasks
 
+import com.google.common.collect.MapMaker
 import io.github.pr0methean.ochd.ImageProcessingStats
 import io.github.pr0methean.ochd.tasks.caching.TaskCache
 import io.github.pr0methean.ochd.tasks.caching.noopTaskCache
 import javafx.scene.image.Image
+import java.util.*
 
 abstract class AbstractImageTask(override val name: String, cache: TaskCache<Image>,
                                  open val stats: ImageProcessingStats)
     : SimpleTask<Image>(name, cache), ImageTask {
     override suspend fun mergeWithDuplicate(other: Task<Image>): ImageTask {
         return super.mergeWithDuplicate(other) as ImageTask
+    }
+
+    private val opaqueRepaints = Collections.newSetFromMap(MapMaker().weakKeys().makeMap<ImageTask,Boolean>())
+
+    override fun opaqueRepaints(): Iterable<ImageTask> = opaqueRepaints
+
+    override fun addOpaqueRepaint(repaint: ImageTask) {
+        opaqueRepaints.add(repaint)
     }
 
     override val asPng: PngCompressionTask by lazy { PngCompressionTask(this, noopTaskCache(), stats) }
