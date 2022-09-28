021b98daef0e1950abeacce96ebe1da16c3fb507
Chris Hennick <hennickc@amazon.com>
2022-09-28 15:00:14 -0700

    Disable strong/soft caching for images over 16 MPx

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
@@ -1,7 +1,9 @@
 package io.github.pr0methean.ochd.tasks.caching
 
 import com.github.benmanes.caffeine.cache.Cache
+import javafx.scene.image.Image
 
+const val MAX_IMAGE_PIXELS_TO_CACHE_NONWEAKLY = 1L.shl(24)
 class SemiStrongTaskCache<T>(name: String, private val backingCache: Cache<SemiStrongTaskCache<*>, Result<*>>):
         WeakTaskCache<T>(name) {
     override var enabled: Boolean
@@ -22,6 +24,14 @@ class SemiStrongTaskCache<T>(name: String, private val backingCache: Cache<SemiS
         if (value == null) {
             backingCache.invalidate(this)
         } else {
+            if (value.isSuccess) {
+                val successValue = value.getOrThrow()
+                if (successValue is Image
+                        && (successValue.height * successValue.width.toLong()) > MAX_IMAGE_PIXELS_TO_CACHE_NONWEAKLY ) {
+                    super.enabled = false
+                    return
+                }
+            }
             backingCache.put(this, value)
             super.enabledSet(value)
         }
