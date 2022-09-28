d6c6a32131196042f113bbe5b1d141412fad6f02
Chris Hennick <hennickc@amazon.com>
2022-09-27 20:15:21 -0700

    Code cleanup: make StainedGlassFront an instance of DyedBlock

--- src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/StainedGlassFront.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/materials/block/pickaxe/StainedGlassFront.kt
@@ -1,28 +1,22 @@
 package io.github.pr0methean.ochd.materials.block.pickaxe
 
+import io.github.pr0methean.ochd.LayerListBuilder
 import io.github.pr0methean.ochd.TaskPlanningContext
-import io.github.pr0methean.ochd.materials.DYES
-import io.github.pr0methean.ochd.tasks.OutputTask
-import io.github.pr0methean.ochd.tasks.RepaintTask
-import io.github.pr0methean.ochd.tasks.caching.noopTaskCache
-import io.github.pr0methean.ochd.texturebase.Material
+import io.github.pr0methean.ochd.tasks.ImageTask
+import io.github.pr0methean.ochd.texturebase.DyedBlock
 import javafx.scene.paint.Color
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 /**
  * This is the only material that feeds an ImageStackingTask to a RepaintTask rather than vice-versa.
  */
-object StainedGlassFront: Material {
+object StainedGlassFront: DyedBlock("stained_glass") {
+    override suspend fun LayerListBuilder.createTextureLayers(color: Color) {
+        layer(sharedLayersTaskRef.get()!!, color)
+    }
 
-    override suspend fun outputTasks(ctx: TaskPlanningContext): Flow<OutputTask> = flow {
-        val masterTask = ctx.stack {
-            background(Color.BLACK, 0.25)
-            layer("borderSolid", Color.BLACK)
-            layer("streaks", Color.BLACK)
-        }
-        DYES.forEach { (name, color) ->
-            emit(ctx.out(RepaintTask(masterTask, color, 1.0, noopTaskCache(), ctx.stats), "block/${name}_stained_glass"))
-        }
+    override suspend fun createSharedLayersTask(ctx: TaskPlanningContext): ImageTask = ctx.stack {
+        background(Color.BLACK, 0.25)
+        layer("borderSolid", Color.BLACK)
+        layer("streaks", Color.BLACK)
     }
 }
