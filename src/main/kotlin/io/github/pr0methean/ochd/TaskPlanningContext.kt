dfa3daa46933bcb96b622c4279f78b1144ab0e6a
Chris Hennick <hennickc@amazon.com>
2022-09-28 14:32:21 -0700

    Test with size limit equivalent to 1.25GiB

--- src/main/kotlin/io/github/pr0methean/ochd/TaskPlanningContext.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/TaskPlanningContext.kt
@@ -21,7 +21,7 @@ fun color(web: String, alpha: Double): Color = Color.web(web, alpha)
 
 private val logger = LogManager.getLogger("TaskPlanningContext")
 // Soft-ref cache will be able to contain this * 16 MPx
-private const val MINIMUM_CACHE_4096x4096 = 32L
+private const val MINIMUM_CACHE_4096x4096 = 20L
 
 /**
  * Holds info needed to build and deduplicate the task graph. Needs to become unreachable once the graph is built.
