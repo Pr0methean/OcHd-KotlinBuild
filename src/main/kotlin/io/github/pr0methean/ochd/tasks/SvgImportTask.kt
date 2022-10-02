6ebe22c5a08e9baf6e36b51791c46988e5f6f3ee
Chris Hennick <hennickc@amazon.com>
2022-10-02 14:50:06 -0700

    Bug fix for awaitFreeMemory in SvgImportTask

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/SvgImportTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/SvgImportTask.kt
@@ -61,11 +61,11 @@ class SvgImportTask(
         stats.onTaskLaunched("SvgImportTask", name)
         val transcoder = batikTranscoder.get()
         val input = TranscoderInput(file.toURI().toString())
-        doJfx("Reserve memory for import of $name") {
-            awaitFreeMemory(16 * width.toLong() * width, name) // Height may be up to width*4
-        }
         val image = SwingFXUtils.toFXImage(transcoder.mutex.withLock {
             transcoder.setTranscodingHints(mapOf(SVGAbstractTranscoder.KEY_WIDTH to width.toFloat()))
+            doJfx("Reserve memory for import of $name") {
+                awaitFreeMemory(16 * width.toLong() * width, name) // Height may be up to width*4
+            }
             transcoder.transcode(input, null)
             transcoder.takeLastImage()!!
         }, null)
