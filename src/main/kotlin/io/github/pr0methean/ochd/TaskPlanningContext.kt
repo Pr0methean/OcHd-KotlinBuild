9a8a369709420780a77eaf91a7b4f001fa6e0595
Chris Hennick <hennickc@amazon.com>
2022-10-02 17:37:58 -0700

    Revert "Revert "Test with even smaller cache""
    This reverts commit 0a723151676b9db1895cb3829645c46026b2a089.

--- src/main/kotlin/io/github/pr0methean/ochd/TaskPlanningContext.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/TaskPlanningContext.kt
@@ -23,7 +23,7 @@ fun color(web: String, alpha: Double): Color = Color.web(web, alpha)
 
 private val logger = LogManager.getLogger("TaskPlanningContext")
 // Soft-ref cache will be able to contain this * 16 MPx
-private const val MINIMUM_CACHE_4096x4096 = 13L
+private const val MINIMUM_CACHE_4096x4096 = 12L
 
 /**
  * Holds info needed to build and deduplicate the task graph. Needs to become unreachable once the graph is built.
