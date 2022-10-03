9b9c470b022f4466d15ac760834f1e2a45757059
Chris Hennick <hennickc@amazon.com>
2022-10-02 19:16:32 -0700

    Test with parallelism of 8

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -17,7 +17,7 @@ import kotlin.system.exitProcess
 import kotlin.system.measureNanoTime
 
 private val logger = LogManager.getRootLogger()
-private const val PARALLELISM = 6
+private const val PARALLELISM = 8
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Suppress("UnstableApiUsage", "DeferredResultUnused")
