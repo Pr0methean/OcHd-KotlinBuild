26fd899d25d9d1fc8109410b9c475acb229ea40b
Chris Hennick <hennickc@amazon.com>
2022-10-08 14:28:59 -0700

    Test using 1<<16 instead of 1<<20

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -20,7 +20,7 @@ private val logger = LogManager.getRootLogger()
 private const val PARALLELISM = 2
 private const val MAX_TILE_SIZE_FOR_PARALLEL_COMMAND_BLOCKS = 1.shl(10)
 
-private const val COMMAND_BLOCK_PRIORITY_ADJUSTMENT = -(1.shl(20)).toDouble()
+private const val COMMAND_BLOCK_PRIORITY_ADJUSTMENT = -(1.shl(16)).toDouble()
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Suppress("UnstableApiUsage", "DeferredResultUnused")
