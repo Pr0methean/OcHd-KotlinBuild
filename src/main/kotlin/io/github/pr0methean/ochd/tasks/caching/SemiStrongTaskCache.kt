ca6127579504a2e594099dd687e578e785c1a5e8
Chris Hennick <hennickc@amazon.com>
2022-10-10 16:58:13 -0700

    Bug fix: redundant logic for disabling SemiStrongTaskCache

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
@@ -4,15 +4,6 @@ import com.github.benmanes.caffeine.cache.Cache
 
 class SemiStrongTaskCache<T>(private val baseCache: AbstractTaskCache<T>, private val backingCache: Cache<SemiStrongTaskCache<*>, Result<*>>):
         AbstractTaskCache<T>(baseCache.name) {
-    override var enabled: Boolean
-        get() = super.enabled
-        set(value) {
-            super.enabled = value
-            if (!value) {
-                backingCache.invalidate(this)
-            }
-        }
-
     @Suppress("UNCHECKED_CAST")
     override fun getNow(): Result<T>? {
         return backingCache.getIfPresent(this) as Result<T>? ?: baseCache.getNow()
