3f956172a708bda0fc1b722d50bb03b5f1cc7754
Chris Hennick <hennickc@amazon.com>
2022-10-17 11:00:48 -0700

    Refactor: duplicate properties

--- src/main/kotlin/io/github/pr0methean/ochd/texturebase/SingleLayerItem.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/texturebase/SingleLayerItem.kt
@@ -3,8 +3,8 @@ package io.github.pr0methean.ochd.texturebase
 import javafx.scene.paint.Paint
 
 open class SingleLayerItem(
-    override val sourceFileName: String,
-    override val name: String,
-    override val color: Paint? = null,
-    override val alpha: Double = 1.0
-) : SingleLayerMaterial("item", sourceFileName, color, alpha)
+    sourceFileName: String,
+    name: String,
+    color: Paint? = null,
+    alpha: Double = 1.0
+) : SingleLayerMaterial(name, "item", sourceFileName, color, alpha)
--- src/main/kotlin/io/github/pr0methean/ochd/texturebase/SingleLayerMaterial.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/texturebase/SingleLayerMaterial.kt
@@ -8,10 +8,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
 abstract class SingleLayerMaterial(
+    override val name: String,
     override val directory: String,
-    open val sourceFileName: String,
-    open val color: Paint? = null,
-    open val alpha: Double = 1.0
+    val sourceFileName: String,
+    val color: Paint? = null,
+    val alpha: Double = 1.0
 ) : SingleTextureMaterial {
     override suspend fun LayerListBuilder.createTextureLayers() {
         layer(sourceFileName, color, alpha)
