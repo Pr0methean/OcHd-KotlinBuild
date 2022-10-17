3ab4f5f3451200bfb5e81139e970ce63e0246975
Chris Hennick <hennickc@amazon.com>
2022-10-16 20:43:24 -0700

    Refactor: can call clear() instead of creating WeakReference(null), and can use a shared nullRef

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/AbstractTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/AbstractTaskCache.kt
@@ -1,7 +1,9 @@
 package io.github.pr0methean.ochd.tasks.caching
 
 import org.apache.logging.log4j.LogManager
+import java.lang.ref.WeakReference
 
+val nullRef = WeakReference<Nothing?>(null)
 private val logger = LogManager.getLogger("AbstractTaskCache")
 abstract class AbstractTaskCache<T>(val name: String) : TaskCache<T> {
     @Volatile override var enabled: Boolean = false
