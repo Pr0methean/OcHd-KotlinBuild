d3378eca21a5ae3ea01bdaefb6b845339b1e13dc
Chris Hennick <hennickc@amazon.com>
2022-10-17 11:00:48 -0700

    Refactor: duplicate properties

--- src/main/kotlin/io/github/pr0methean/ochd/texturebase/SingleLayerParticle.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/texturebase/SingleLayerParticle.kt
@@ -3,8 +3,8 @@ package io.github.pr0methean.ochd.texturebase
 import javafx.scene.paint.Paint
 
 class SingleLayerParticle(
-    override val sourceFileName: String,
-    override val name: String,
-    override val color: Paint? = null,
-    override val alpha: Double = 1.0):
-    SingleLayerMaterial("particle", sourceFileName, color, alpha)
+    sourceFileName: String,
+    name: String,
+    color: Paint? = null,
+    alpha: Double = 1.0
+): SingleLayerMaterial(name, "particle", sourceFileName, color, alpha)
