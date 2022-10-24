6085aaf031370bd7deb9545f22a61bf6607f4e78
Chris Hennick <hennickc@amazon.com>
2022-10-23 20:46:45 -0700

    Refactor: can use an AtomicBoolean to ensure we only launch cleanUp() once per key

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
@@ -3,6 +3,7 @@ package io.github.pr0methean.ochd.tasks.caching
 import com.github.benmanes.caffeine.cache.Cache
 import java.lang.ref.Cleaner
 import java.lang.ref.WeakReference
+import java.util.concurrent.atomic.AtomicBoolean
 
 private val CLEANER = Cleaner.create { runnable ->
     val thread = Thread.ofVirtual().unstarted(runnable)
@@ -11,6 +12,7 @@ private val CLEANER = Cleaner.create { runnable ->
 }
 class SemiStrongTaskCache<T>(private val baseCache: AbstractTaskCache<T>, private val backingCache: Cache<SemiStrongTaskCache<*>, Result<*>>):
         AbstractTaskCache<T>(baseCache.name) {
+    val cleanupScheduled = AtomicBoolean(false)
     @Suppress("UNCHECKED_CAST")
     override fun getNow(): Result<T>? {
         return backingCache.getIfPresent(this) as Result<T>? ?: baseCache.getNow()
@@ -28,8 +30,10 @@ class SemiStrongTaskCache<T>(private val baseCache: AbstractTaskCache<T>, privat
 
     override fun enabledSet(value: Result<T>) {
         val backingCacheRef = WeakReference(backingCache)
-        CLEANER.register(this) {
-            backingCacheRef.get()?.cleanUp()
+        if (cleanupScheduled.compareAndSet(false, true)) {
+            CLEANER.register(this) {
+                backingCacheRef.get()?.cleanUp()
+            }
         }
         if (value.isSuccess && (baseCache is WeakTaskCache || baseCache is SoftTaskCache)) {
             CLEANER.register(value.getOrThrow()) {
