b5558a7e58726457d6bc2fbb1b6e7f588b000451
Chris Hennick <hennickc@amazon.com>
2022-11-05 18:16:06 -0700

    Test without java.lang.ref.Cleaner

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
@@ -1,18 +1,9 @@
 package io.github.pr0methean.ochd.tasks.caching
 
 import com.github.benmanes.caffeine.cache.Cache
-import java.lang.ref.Cleaner
-import java.lang.ref.WeakReference
-import java.util.concurrent.atomic.AtomicBoolean
 
-private val CLEANER = Cleaner.create { runnable ->
-    val thread = Thread.ofVirtual().unstarted(runnable)
-    thread.priority = Thread.MAX_PRIORITY
-    thread
-}
 class SemiStrongTaskCache<T>(private val victimCache: AbstractTaskCache<T>, private val primaryCache: Cache<SemiStrongTaskCache<*>, Result<*>>):
         AbstractTaskCache<T>(victimCache.name) {
-    private val cleanupScheduled = AtomicBoolean(false)
     @Suppress("UNCHECKED_CAST")
     override fun getNow(): Result<T>? {
         return primaryCache.getIfPresent(this) as Result<T>? ?: victimCache.getNow()
@@ -29,16 +20,6 @@ class SemiStrongTaskCache<T>(private val victimCache: AbstractTaskCache<T>, priv
     }
 
     override fun enabledSet(value: Result<T>) {
-        val backingCacheRef = WeakReference(primaryCache)
-        val cleanUpCacheRunnable = Runnable {
-            backingCacheRef.get()?.cleanUp()
-        }
-        if (cleanupScheduled.compareAndSet(false, true)) {
-            CLEANER.register(this, cleanUpCacheRunnable)
-        }
-        if (victimCache is WeakTaskCache || victimCache is SoftTaskCache) {
-            CLEANER.register(value, cleanUpCacheRunnable)
-        }
         victimCache.enabledSet(value)
         primaryCache.put(this, value)
     }
