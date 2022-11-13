b0dcdaaa1d98137240ec7f1fb6d3bc354b2b158d
Chris Hennick <hennickc@amazon.com>
2022-11-13 11:19:15 -0800

    Test reverting huge-tile cache to last known good size

--- src/main/kotlin/io/github/pr0methean/ochd/TaskPlanningContext.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/TaskPlanningContext.kt
@@ -31,7 +31,7 @@ private val logger = LogManager.getLogger("TaskPlanningContext")
 // Main Caffeine cache will be able to contain this * 16 MPx * 4 bytes/Px
 private const val MINIMUM_CACHE_4096x4096 = 20L
 // Huge-tile Caffeine cache will be able to contain this * 64 MPx * 4 bytes/Px
-private const val MINIMUM_CACHE_16384x4096 = 4L
+private const val MINIMUM_CACHE_16384x4096 = 3L
 
 /**
  * Holds info needed to build and deduplicate the task graph. Needs to become unreachable once the graph is built.
