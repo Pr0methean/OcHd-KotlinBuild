66b23ad9a39ea5028a8d22ff3520230e2d18ae5a
Chris Hennick <hennickc@amazon.com>
2022-11-26 14:47:16 -0800

    Test sorting by unstarted-and-unavailable subtasks first

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -31,6 +31,7 @@ import kotlin.system.measureNanoTime
 
 private const val CAPACITY_PADDING_FACTOR = 2
 private val taskOrderComparator = comparingLong(OutputTask::timesFailed)
+    .then(comparingInt {it.totalSubtasks - it.startedOrAvailableSubtasks()})
     .then(comparingInt(OutputTask::startedOrAvailableSubtasks).reversed())
     .then(comparingInt(OutputTask::unstartedCacheableSubtasks))
 private val logger = LogManager.getRootLogger()
