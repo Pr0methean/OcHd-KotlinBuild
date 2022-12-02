68307aad1e9de54b406cd88471aa2cd320ae4158
Chris Hennick <hennickc@amazon.com>
2022-12-02 12:17:41 -0800

    Refactor: reuse ByteArrayOutputStream

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/PngCompressionTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/PngCompressionTask.kt
@@ -4,22 +4,34 @@ import io.github.pr0methean.ochd.ImageProcessingStats
 import io.github.pr0methean.ochd.tasks.caching.TaskCache
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.image.Image
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asContextElement
+import kotlinx.coroutines.plus
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
 
 const val PNG_PRESIZE: Int = 512*1024
+private val THREAD_LOCAL_BAOS = ThreadLocal.withInitial {ByteArrayOutputStream(PNG_PRESIZE)}
 @Suppress("FunctionName")
-fun PngCompressionTask(
+class PngCompressionTask(
     base: AbstractTask<Image>, cache: TaskCache<ByteArray>, stats: ImageProcessingStats
-): TransformingTask<Image, ByteArray> = TransformingTask(
+): TransformingTask<Image, ByteArray>(
     "PNG compression of $base", base = base, cache = cache, transform = { image ->
-    ByteArrayOutputStream(PNG_PRESIZE).use {
-        stats.onTaskLaunched("PngCompressionTask", base.name)
-        @Suppress("BlockingMethodInNonBlockingContext")
+    THREAD_LOCAL_BAOS.get().run {
+        try {
+            stats.onTaskLaunched("PngCompressionTask", base.name)
+            @Suppress("BlockingMethodInNonBlockingContext")
 
-        ImageIO.write(SwingFXUtils.fromFXImage(image, null), "PNG", it)
-        val packed = it.toByteArray()
-        stats.onTaskCompleted("PngCompressionTask", base.name)
-        packed
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "PNG", this)
+            val packed = toByteArray()
+            stats.onTaskCompleted("PngCompressionTask", base.name)
+            packed
+        } finally {
+            reset()
+        }
     }
-})
+}) {
+    override suspend fun createCoroutineScope(): CoroutineScope {
+        return super.createCoroutineScope().plus(THREAD_LOCAL_BAOS.asContextElement())
+    }
+}
