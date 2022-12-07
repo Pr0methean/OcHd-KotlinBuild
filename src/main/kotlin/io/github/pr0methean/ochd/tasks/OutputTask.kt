e051db566fd68ed2b7a5d74c53134d31aefac08c
Chris Hennick <hennickc@amazon.com>
2022-12-07 12:05:46 -0800

    Don't need to use IO dispatcher since total OutputTask count is limited

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/OutputTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/OutputTask.kt
@@ -2,9 +2,6 @@ package io.github.pr0methean.ochd.tasks
 
 import io.github.pr0methean.ochd.ImageProcessingStats
 import io.github.pr0methean.ochd.tasks.caching.noopTaskCache
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.io.File
 import java.nio.file.Files
 
@@ -17,19 +14,17 @@ class OutputTask(
 ): TransformingTask<ByteArray, Unit>("Output $name", source, noopTaskCache()) {
     override suspend fun transform(input: ByteArray) {
         stats.onTaskLaunched("OutputTask", name)
-        withContext(Dispatchers.IO.plus(CoroutineName(name))) {
-            val firstFile = files[0]
-            firstFile.parentFile?.mkdirs()
-            val firstFilePath = firstFile.absoluteFile.toPath()
-            Files.write(firstFilePath, input)
-            if (files.size > 1) {
-                for (file in files.subList(1, files.size)) {
-                    file.parentFile?.mkdirs()
-                    Files.copy(firstFilePath, file.absoluteFile.toPath())
-                }
+        val firstFile = files[0]
+        firstFile.parentFile?.mkdirs()
+        val firstFilePath = firstFile.absoluteFile.toPath()
+        Files.write(firstFilePath, input)
+        if (files.size > 1) {
+            for (file in files.subList(1, files.size)) {
+                file.parentFile?.mkdirs()
+                Files.copy(firstFilePath, file.absoluteFile.toPath())
             }
-            stats.onTaskCompleted("OutputTask", name)
         }
+        stats.onTaskCompleted("OutputTask", name)
     }
 
     val isCommandBlock: Boolean = name.contains("command_block")
