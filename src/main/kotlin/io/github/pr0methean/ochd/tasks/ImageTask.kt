9bfb5d42f3ae018fc088969635f3914b17473901
Chris Hennick <hennickc@amazon.com>
2022-12-08 15:44:48 -0800

    Use zip -9 for texture pack For downloaders' convenience, even if the compute cost exceeds the storage savings

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/ImageTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/ImageTask.kt
@@ -5,10 +5,14 @@ import javafx.scene.image.Image
 import org.apache.logging.log4j.util.StringBuilderFormattable
 
 
+/** Specialization of [Task]&lt;[Image]&gt;. */
 interface ImageTask : StringBuilderFormattable, Task<Image> {
     val asPng: TransformingTask<Image, ByteArray>
     override suspend fun mergeWithDuplicate(other: Task<*>): ImageTask
 
+    /**
+     * These can be used by [RepaintTask] to repaint a repaint if it's cached or in progress and the original isn't.
+     */
     fun opaqueRepaints(): Iterable<ImageTask>
 
     fun addOpaqueRepaint(repaint: ImageTask)
