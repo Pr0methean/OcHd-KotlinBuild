377b203e75b7661a25b8294ea009adce726c1a55
Chris Hennick <hennickc@amazon.com>
2022-12-08 11:43:54 -0800

    Clarify comment

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/TaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/TaskCache.kt
@@ -1,8 +1,10 @@
 package io.github.pr0methean.ochd.tasks.caching
 
 /**
- * Stores the output of a particular task for reuse, but can usually be cleared if the memory is needed. May be backed
- * by an evictable entry in a Caffeine cache and/or a soft or weak reference.
+ * Stores the output of a particular [io.github.pr0methean.ochd.tasks.Task] for reuse, but can usually be cleared if the
+ * memory is needed. May be backed by an evictable entry in a Caffeine cache and/or a soft or weak reference. A separate
+ * instance is needed for each Task (unless it uses [NoopTaskCache]), and that Task should be the only object that
+ * references it strongly.
  */
 interface TaskCache<T> {
     var enabled: Boolean
