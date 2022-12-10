83b7a916f6c97b9a80245dc7db0346686695cb7c
Chris Hennick <hennickc@amazon.com>
2022-12-10 15:51:47 -0800

    Refactor

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SemiStrongTaskCache.kt
@@ -4,8 +4,10 @@ import com.github.benmanes.caffeine.cache.Cache
 
 /**
  * A TaskCache that's backed by a Caffeine cache (the primary cache) and another TaskCache instance (the victim cache).
- * The primary cache should have hard references to values and be limited in size. The victim cache should be a
- * [SoftTaskCache], [WeakTaskCache] or [NoopTaskCache].
+ * So named because of the primary cache is limited in size and the victim cache is a [SoftTaskCache] or [WeakTaskCache]
+ * then the total number of cached values will have a minimum defined in code but a maximum that depends on heap size,
+ * yielding a cache hit rate that scales almost optimally with the available heap space (i.e. the amount of space not
+ * occupied by strongly-reachable objects).
  */
 class SemiStrongTaskCache<T>(
         private val victimCache: TaskCache<T>,
@@ -29,9 +31,5 @@ class SemiStrongTaskCache<T>(
     override fun enabledSet(value: T) {
         victimCache.enabledSet(value)
         primaryCache.put(this, value)
-    }
-
-    fun clearPrimaryCache() {
-        primaryCache.invalidateAll()
     }
 }
