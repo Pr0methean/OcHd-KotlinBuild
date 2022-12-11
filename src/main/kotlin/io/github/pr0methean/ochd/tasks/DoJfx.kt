ed06c142b1e9173f8d78e8836e2df11c89c9e47b
Chris Hennick <hennickc@amazon.com>
2022-12-10 21:11:53 -0800

    Test with inline doJfx

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/DoJfx.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/DoJfx.kt
@@ -7,24 +7,26 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
+import java.nio.charset.Charset
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 
-private val LOGGER = LogManager.getLogger("doJfx")
-private val DEFAULT_ERR = System.err
-private val DEFAULT_CHARSET = DEFAULT_ERR.charset()
-private val ERR_CATCHER = ByteArrayOutputStream()
-private val ERR_CATCHER_STREAM = PrintStream(ERR_CATCHER, true, DEFAULT_CHARSET)
-private val SYSERR_SWITCHED = AtomicBoolean(false)
+val LOGGER: Logger = LogManager.getLogger("doJfx")
+val DEFAULT_ERR: PrintStream = System.err
+val DEFAULT_CHARSET: Charset = DEFAULT_ERR.charset()
+val ERR_CATCHER: ByteArrayOutputStream = ByteArrayOutputStream()
+val ERR_CATCHER_STREAM: PrintStream = PrintStream(ERR_CATCHER, true, DEFAULT_CHARSET)
+val SYSERR_SWITCHED: AtomicBoolean = AtomicBoolean(false)
 
 /**
  * Dispatches the given block of code to the JavaFX rendering thread, waits for it to execute, and checks for errors
  * logged to System.err while running.
  */
 @Suppress("BlockingMethodInNonBlockingContext")
-suspend fun <T> doJfx(name: String, jfxCode: CoroutineScope.() -> T): T = runCatching {
+suspend inline fun <T> doJfx(name: String, crossinline jfxCode: CoroutineScope.() -> T): T = runCatching {
     if (SYSERR_SWITCHED.compareAndSet(false, true)) {
         System.setErr(ERR_CATCHER_STREAM)
     }
