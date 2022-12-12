11cf67ff38d1d884f3e8ad50446a0bf5a74be961
Chris Hennick <hennickc@amazon.com>
2022-12-11 20:38:11 -0800

    Revert "Test of changing soft references to weak on disable, but only if not already a WeakReference"
    This reverts commit 9a4db62c55f4f9b16a16822ada5408fa682f9aef.

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/VictimReferenceDeferredTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/VictimReferenceDeferredTaskCache.kt
@@ -28,12 +28,6 @@ class VictimReferenceDeferredTaskCache<T>(
     override fun disable(): Boolean {
         val disabledPrimary = primaryCache.disable()
         val disabledSuper = super.disable()
-        if (disabledSuper) {
-            val ref = coroutineRef.get()
-            if (ref !is WeakReference<*>) {
-                coroutineRef.compareAndSet(ref, WeakReference(ref.get()))
-            }
-        }
         return disabledPrimary || disabledSuper
     }
 
