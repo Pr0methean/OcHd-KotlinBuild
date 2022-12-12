8beeadce71663174a9c271ed0125330e966e5354
Chris Hennick <hennickc@amazon.com>
2022-12-11 18:05:08 -0800

    Fix style issue

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/DeferredTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/DeferredTaskCache.kt
@@ -7,8 +7,8 @@ import java.util.concurrent.atomic.AtomicBoolean
  * Stores the coroutine running a particular [io.github.pr0methean.ochd.tasks.Task] so multiple consuming jobs can await
  * the same coroutine and/or reuse its result. May be backed by an evictable entry in a Caffeine cache
  * and/or a soft or weak reference, to support memory-constrained reuse. A separate
- * instance is needed for each Task (unless it uses [NoopDeferredTaskCache]), and that Task should be the only object that
- * references it strongly.
+ * instance is needed for each Task (unless it uses [NoopDeferredTaskCache]), and that Task (and the DeferredTaskCache's
+ * own members) should be the only objects that reference the coroutine once it's completed.
  */
 abstract class DeferredTaskCache<out T> {
     private val enabled = AtomicBoolean(false)
