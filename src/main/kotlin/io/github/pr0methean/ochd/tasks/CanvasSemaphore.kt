83204b75ded423a8245edb6808fd6574b07ba7c5
Chris Hennick <hennickc@amazon.com>
2022-12-14 08:56:47 -0800

    Detekt fix

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/CanvasSemaphore.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/CanvasSemaphore.kt
@@ -2,8 +2,10 @@ package io.github.pr0methean.ochd.tasks
 
 import kotlinx.coroutines.sync.Semaphore
 
+const val MAX_CANVASES: Int = 8
+
 /**
  * Taken when creating a canvas, and released when it becomes unreachable. Keeps the number of canvases in the live set
  * under control.
  */
-val CANVAS_SEMAPHORE = Semaphore(8)
+val CANVAS_SEMAPHORE: Semaphore = Semaphore(MAX_CANVASES)
