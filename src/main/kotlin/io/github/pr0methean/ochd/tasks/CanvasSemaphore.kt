292aed3ac0294420ad7a5d7f596aef27f8fb230e
Chris Hennick <hennickc@amazon.com>
2022-12-14 08:54:52 -0800

    Test with 8 permits to prevent deadlock

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/CanvasSemaphore.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/CanvasSemaphore.kt
@@ -6,4 +6,4 @@ import kotlinx.coroutines.sync.Semaphore
  * Taken when creating a canvas, and released when it becomes unreachable. Keeps the number of canvases in the live set
  * under control.
  */
-val CANVAS_SEMAPHORE = Semaphore(Runtime.getRuntime().availableProcessors())
+val CANVAS_SEMAPHORE = Semaphore(8)
