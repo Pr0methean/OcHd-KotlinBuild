17fbefea6600c1014d09c1091108fe05c5056674
Chris Hennick <hennickc@amazon.com>
2022-12-22 16:21:18 -0800

    Revert "Bug fix? Call cleanUp to ensure huge-tile cache is actually empty before GC"
    This reverts commit affba2bc0844cfc48515175447bb8793669edcf3.

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -98,7 +98,6 @@ suspend fun main(args: Array<String>) {
         runAll(cbTasks, scope, stats, MAX_HUGE_TILE_OUTPUT_TASKS)
         stats.readHugeTileCache(hugeTaskCache)
         hugeTaskCache.invalidateAll()
-        hugeTaskCache.cleanUp()
         gcIfUsingLargeTiles(tileSize)
         runAll(nonCbTasks, scope, stats, MAX_OUTPUT_TASKS)
     }
