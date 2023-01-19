e98e681b9a2bb2a78f7065471b17beb5a23a7d9a
Chris Hennick <hennickc@amazon.com>
2023-01-19 14:10:02 -0800

    Bug fix: witness value may be null even when final value isn't

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/HardTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/HardTaskCache.kt
@@ -18,7 +18,8 @@ class HardTaskCache<T>(
     @Suppress("OVERRIDE_BY_INLINE")
     override inline fun computeIfAbsent(coroutineCreator: () -> Deferred<T>): Deferred<T> {
         return if (isEnabled()) {
-            coroutineRef.compareAndExchange(null, coroutineCreator())!!
+            val newCoroutine = coroutineCreator()
+            coroutineRef.compareAndExchange(null, newCoroutine) ?: newCoroutine
         } else coroutineCreator()
     }
 }
