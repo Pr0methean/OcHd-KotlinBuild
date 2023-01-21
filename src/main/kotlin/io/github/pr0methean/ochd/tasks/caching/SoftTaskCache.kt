39966a25c57e9d219015a26e2a8b505081ed293c
Chris Hennick <hennickc@amazon.com>
2023-01-21 13:33:55 -0800

    Keep cached results weakly reachable after clearing

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SoftTaskCache.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/caching/SoftTaskCache.kt
@@ -1,7 +1,9 @@
 package io.github.pr0methean.ochd.tasks.caching
 
 import kotlinx.coroutines.Deferred
+import java.lang.ref.Reference
 import java.lang.ref.SoftReference
+import java.lang.ref.WeakReference
 import java.util.concurrent.atomic.AtomicReference
 
 val nullReference: SoftReference<Nothing?> = SoftReference<Nothing?>(null)
@@ -13,16 +15,23 @@ val nullReference: SoftReference<Nothing?> = SoftReference<Nothing?>(null)
 class SoftTaskCache<T>(
     name: String
 ): DeferredTaskCache<T>(name) {
-    val coroutineRef: AtomicReference<SoftReference<out Deferred<T>?>> = AtomicReference(nullReference)
+    val coroutineRef: AtomicReference<Reference<out Deferred<T>?>> = AtomicReference(nullReference)
     override fun getNowAsync(): Deferred<T>? = coroutineRef.get().get()
 
     override fun clear() {
-        coroutineRef.getAndSet(nullReference).clear()
+        coroutineRef.updateAndGet {
+            val current = it.get()
+            if (current == null) {
+                nullReference
+            } else if (it is WeakReference) {
+                it
+            } else WeakReference(current)
+        }
     }
 
     @Suppress("DeferredIsResult", "OVERRIDE_BY_INLINE")
     override inline fun computeIfAbsent(crossinline coroutineCreator: () -> Deferred<T>): Deferred<T> {
-        var currentCoroutineRef: SoftReference<out Deferred<T>?> = coroutineRef.get()
+        var currentCoroutineRef: Reference<out Deferred<T>?> = coroutineRef.get()
         val newCoroutine by lazy { coroutineCreator() }
         val newCoroutineRef by lazy { SoftReference(newCoroutine) }
         while (true) {
