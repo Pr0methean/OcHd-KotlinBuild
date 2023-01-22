417906f06b651517dce5da11c10183fa224f339b
Chris Hennick <hennickc@amazon.com>
2023-01-21 16:34:50 -0800

    Optimize netAddedToCache for tasks with caching already disabled

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/AbstractTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/AbstractTask.kt
@@ -89,11 +89,13 @@ abstract class AbstractTask<out T>(
     }
 
     suspend fun netAddedToCache(): Int {
-        var netAdded = if (isStartedOrAvailable()) {
+        var netAdded = if (!cache.isEnabled()) {
+            0
+        } else if (isStartedOrAvailable()) {
             if (mutex.withLock { directDependentTasks.size == 1 }) -1 else 0
-        } else {
-            if (cache.isEnabled() && mutex.withLock { directDependentTasks.size >= 2 }) 1 else 0
-        }
+        } else if (mutex.withLock { directDependentTasks.size >= 2 }) {
+            1
+        } else 0
         for (task in directDependencies) {
             netAdded += task.netAddedToCache()
         }
