26d2cc2190bcfde9bdff1a430f1e2efddde701ee
Chris Hennick <hennickc@amazon.com>
2023-01-27 15:07:20 -0800

    Test with exponent of 0.25

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/AbstractTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/AbstractTask.kt
@@ -92,7 +92,7 @@ abstract class AbstractTask<out T>(
         var coefficient = if (!cache.isEnabled()) {
             0.0
         } else if (isStartedOrAvailable()) {
-            Math.scalb(1.0, 1 - mutex.withLock { directDependentTasks.count { !it.isStartedOrAvailable() } })
+            Math.scalb(1.0, 2 - 2*mutex.withLock { directDependentTasks.count { !it.isStartedOrAvailable() } })
         } else 0.0
         for (task in directDependencies) {
             coefficient += task.cacheClearingCoefficient()
