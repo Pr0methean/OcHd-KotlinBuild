54ca6144d85a9657169281730de37374dae96451
Chris Hennick <hennickc@amazon.com>
2023-01-30 19:54:08 -0800

    Test raising soft heap limit to 90%

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -50,10 +50,10 @@ private const val MAX_HUGE_TILE_OUTPUT_TASKS_PER_CPU = 4.0
 
 private const val MIN_TILE_SIZE_FOR_EXPLICIT_GC = 2048
 
-private const val SOFT_HEAP_LIMIT = 0.80
+private const val SOFT_HEAP_LIMIT_FRACTION = 0.90
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
-private val softHeapLimitBytes = (memoryMxBean.heapMemoryUsage.max * SOFT_HEAP_LIMIT).toLong()
+private val softHeapLimitBytes = (memoryMxBean.heapMemoryUsage.max * SOFT_HEAP_LIMIT_FRACTION).toLong()
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
 @Suppress("UnstableApiUsage", "DeferredResultUnused")
