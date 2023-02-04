2e9a4010012a803d6fca0e7b5a4cb225c5040c1b
Chris Hennick <hennickc@amazon.com>
2023-02-04 15:17:34 -0800

    Test allowing 5 concurrent tasks

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -40,7 +40,7 @@ private val taskOrderComparator = comparingInt<PngOutputTask> {
 
 private val logger = LogManager.getRootLogger()
 
-private const val MAX_OUTPUT_TASKS_PER_CPU = 2.0
+private const val MAX_OUTPUT_TASKS_PER_CPU = 1.75
 
 private const val MIN_TILE_SIZE_FOR_EXPLICIT_GC = 2048
 
