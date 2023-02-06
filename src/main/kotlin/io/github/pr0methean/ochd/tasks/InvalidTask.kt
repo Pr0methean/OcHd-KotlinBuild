459b384437d6192794ef4e37bf0c00e6fafd3a89
Chris Hennick <hennickc@amazon.com>
2023-02-06 12:37:59 -0800

    Bug fix: missing an override

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/InvalidTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/InvalidTask.kt
@@ -7,11 +7,11 @@ import kotlinx.coroutines.Dispatchers
  * A task that throws an exception if it's ever launched.
  */
 object InvalidTask: AbstractImageTask("InvalidTask", noopDeferredTaskCache(), Dispatchers.Unconfined, 0, 0) {
+    override fun hasColor(): Boolean = false
+
     override val directDependencies: Iterable<AbstractTask<*>> = listOf()
 
     override fun computeHashCode(): Int = 0xDEADBEEF.toInt()
 
-    override suspend fun perform(): Nothing {
-        throw IllegalArgumentException("InvalidTask")
-    }
+    override suspend fun perform(): Nothing = error("InvalidTask")
 }
