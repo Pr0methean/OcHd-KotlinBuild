05e834f85b001266ee8c7182e9b43e19d5f9a621
Chris Hennick <hennickc@amazon.com>
2023-02-09 10:55:13 -0800

    Attempt to limit heap to 75%

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -42,7 +42,7 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
-private const val SOFT_HEAP_LIMIT_FRACTION = 0.80
+private const val SOFT_HEAP_LIMIT_FRACTION = 0.75
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
 private val softHeapLimitBytes = (memoryMxBean.heapMemoryUsage.max * SOFT_HEAP_LIMIT_FRACTION).toLong()
