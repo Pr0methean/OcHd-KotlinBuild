6fc939ac840a553afd2ece1b659f22d5ca4df29e
Chris Hennick <hennickc@amazon.com>
2023-02-10 12:36:34 -0800

    Test a hard-throttling threshold of 0.98

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -55,7 +55,7 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
-private const val HARD_THROTTLING_THRESHOLD = 0.95
+private const val HARD_THROTTLING_THRESHOLD = 0.98
 private const val HARD_THROTTLING_AFTER_GC_THRESHOLD = 0.90
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
