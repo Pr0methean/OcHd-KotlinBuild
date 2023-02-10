ffa818329bf7ea46d785e67881be5eb7fda716c3
Chris Hennick <hennickc@amazon.com>
2023-02-09 18:23:45 -0800

    Test whether 90% threshold improves performance

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -54,7 +54,7 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
-private const val HARD_THROTTLING_THRESHOLD = 0.95
+private const val HARD_THROTTLING_THRESHOLD = 0.90
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
 private val heapSizeBytes = memoryMxBean.heapMemoryUsage.max.toDouble()
