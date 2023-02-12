64554f42fcd9b707ac110c9bf7237f58eecef90d
Chris Hennick <hennickc@amazon.com>
2023-02-11 17:29:19 -0800

    Raise explicit-GC threshold to 80% of heap

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -56,7 +56,7 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
 private const val HARD_THROTTLING_THRESHOLD = 0.93
-private const val EXPLICIT_GC_THRESHOLD = 0.5
+private const val EXPLICIT_GC_THRESHOLD = 0.8
 private const val FREED_PER_GC_TO_SUPPRESS_EXPLICIT_GC = 0.1
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
