ffee7dbb7ba52f7fa8120ee7b2d23aff5c3a4618
Chris Hennick <hennickc@amazon.com>
2023-03-08 09:55:46 -0800

    Make throttling slightly more aggressive

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -47,10 +47,10 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
-private const val FORCE_GC_THRESHOLD = 0.90
-private const val HARD_THROTTLING_THRESHOLD = 0.90
-private const val EXPLICIT_GC_THRESHOLD = 0.75
-private const val FREED_PER_GC_TO_SUPPRESS_EXPLICIT_GC = 0.04
+private const val FORCE_GC_THRESHOLD = 0.85
+private const val HARD_THROTTLING_THRESHOLD = 0.85
+private const val EXPLICIT_GC_THRESHOLD = 0.70
+private const val FREED_PER_GC_TO_SUPPRESS_EXPLICIT_GC = 0.05
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
 private val heapSizeBytes = memoryMxBean.heapMemoryUsage.max.toDouble()
