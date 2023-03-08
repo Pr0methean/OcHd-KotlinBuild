98d1ad902a1f2a107d04f62a098299e5d6b3e758
Chris Hennick <hennickc@amazon.com>
2023-03-08 13:31:31 -0800

    Log a message after yielding

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -238,6 +238,7 @@ suspend fun main(args: Array<String>) {
                         gcIfNeeded()
                     } else if (maxJobs - currentInProgressJobs <= 1) {
                         yield()
+                        logger.info("Yielded from main thread since max of {} jobs were running", box(maxJobs))
                     }
                 }
             }
