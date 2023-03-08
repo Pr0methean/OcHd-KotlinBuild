0b31b1b546b5358ab8173acf4c5826b859f71255
Chris Hennick <hennickc@amazon.com>
2023-03-08 09:30:48 -0800

    Test with minimum of 3 tasks

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -47,10 +47,10 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
-private const val FORCE_GC_THRESHOLD = 0.90
-private const val HARD_THROTTLING_THRESHOLD = 0.90
-private const val EXPLICIT_GC_THRESHOLD = 0.75
-private const val FREED_PER_GC_TO_SUPPRESS_EXPLICIT_GC = 0.04
+private const val FORCE_GC_THRESHOLD = 0.85
+private const val HARD_THROTTLING_THRESHOLD = 0.85
+private const val EXPLICIT_GC_THRESHOLD = 0.70
+private const val FREED_PER_GC_TO_SUPPRESS_EXPLICIT_GC = 0.05
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
 private val heapSizeBytes = memoryMxBean.heapMemoryUsage.max.toDouble()
@@ -60,7 +60,7 @@ private val minClearedPerGcBytes = (heapSizeBytes * FREED_PER_GC_TO_SUPPRESS_EXP
 private val explicitGcThresholdBytes = (heapSizeBytes * EXPLICIT_GC_THRESHOLD).toLong()
 private const val WORKING_BYTES_PER_PIXEL = 50
 val nCpus: Int = Runtime.getRuntime().availableProcessors()
-private const val MIN_OUTPUT_TASKS = 2
+private const val MIN_OUTPUT_TASKS = 3
 
 @Suppress("UnstableApiUsage", "DeferredResultUnused", "NestedBlockDepth", "LongMethod", "ComplexMethod")
 suspend fun main(args: Array<String>) {
