3631ce361ab710bf014e8ae1ca9017769a0fe3c8
Chris Hennick <hennickc@amazon.com>
2023-03-09 15:41:58 -0800

    Test allowing explicit GC if less than 15% is free now or less than 30% was free after last GC

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -48,9 +48,9 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
-private const val FORCE_GC_THRESHOLD = 0.90
+private const val FORCE_GC_THRESHOLD = 0.85
 private const val HARD_THROTTLING_THRESHOLD = 0.85
-private const val EXPLICIT_GC_THRESHOLD = 0.80
+private const val EXPLICIT_GC_THRESHOLD = 0.70
 private val gcMxBean = ManagementFactory.getPlatformMXBeans(GarbageCollectorMXBean::class.java).first()
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
 private val heapSizeBytes = memoryMxBean.heapMemoryUsage.max.toDouble()
