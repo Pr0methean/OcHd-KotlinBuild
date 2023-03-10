a7e646d23c0604c91d76324a6d61efaaf2635881
Chris Hennick <hennickc@amazon.com>
2023-03-10 13:05:24 -0800

    Lower goal cache fraction to 30%

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -44,7 +44,7 @@ private const val MAX_TILE_SIZE_FOR_PRINT_DEPENDENCY_GRAPH = 32
 val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
 
 private const val FORCE_GC_THRESHOLD = 0.85
-private const val GOAL_CACHE_FRACTION_OF_HEAP = 0.4
+private const val GOAL_CACHE_FRACTION_OF_HEAP = 0.3
 private val memoryMxBean = ManagementFactory.getMemoryMXBean()
 private val heapSizeBytes = memoryMxBean.heapMemoryUsage.max.toDouble()
 private val goalCacheSizeBytes = heapSizeBytes * GOAL_CACHE_FRACTION_OF_HEAP
