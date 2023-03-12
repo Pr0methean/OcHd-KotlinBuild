91df6e02fb6946944ad5804165837c7f58d42d1f
Chris Hennick <hennickc@amazon.com>
2023-03-12 13:12:55 -0700

    Test with a minimum of 3 output tasks

--- src/main/kotlin/io/github/pr0methean/ochd/Main.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/Main.kt
@@ -47,7 +47,7 @@ private val heapSizeBytes = memoryMxBean.heapMemoryUsage.max.toDouble()
 private val goalCacheSizeBytes = heapSizeBytes * GOAL_CACHE_FRACTION_OF_HEAP
 private const val BYTES_PER_PIXEL = 4
 val nCpus: Int = Runtime.getRuntime().availableProcessors()
-private const val MIN_OUTPUT_TASKS = 2
+private const val MIN_OUTPUT_TASKS = 3
 private const val MAX_OUTPUT_TASKS_PER_CPU = 2
 private val maxOutputTasks = nCpus * MAX_OUTPUT_TASKS_PER_CPU
 
