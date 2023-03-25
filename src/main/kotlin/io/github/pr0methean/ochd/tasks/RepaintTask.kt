54127af972fc8d6651612dccde0f7487fc999e8b
Chris Hennick <hennickc@amazon.com>
2023-03-25 10:00:05 -0700

    Re-enable RepaintTask replacement now that graph isn't in copyless mode

--- src/main/kotlin/io/github/pr0methean/ochd/tasks/RepaintTask.kt
+++ src/main/kotlin/io/github/pr0methean/ochd/tasks/RepaintTask.kt
@@ -99,19 +99,21 @@ class RepaintTask(
         } else {
             super.perform()
         }
-        /*
-        FIXME: This causes a ConcurrentModificationException
-        if (paint is Color && paint.opacity == 1.0 && cache.isEnabled() && base.cache.isEnabled()
-                && base.directDependentTasks.all { it is RepaintTask }) {
+        try {
+            if (paint is Color && paint.opacity == 1.0 && cache.isEnabled() && base.cache.isEnabled()
+                && base.graph.incomingEdgesOf(base).all { graph.getEdgeSource(it) is RepaintTask }
+            ) {
             /*
              * Painting a black X blue or green has the same result as painting a red X blue or green.
              * Therefore, if the only impending uses of a black X are to repaint it blue and green, we can replace the
              * black X with a red one in cache, if the red one was going to be cached as a pre-painted layer anyway.
              */
-            logger.info("Using repaint {} to replace base image {}", name, base.name)
-            base.cache.setValue(snapshot)
+                logger.info("Using repaint {} to replace base image {}", name, base.name)
+                base.cache.setValue(snapshot)
+            }
+        } catch (ignored: IllegalArgumentException) {
+            // Happens if base has already been removed from the graph. Then there's no cache to update.
         }
-         */
         return snapshot
     }
 
